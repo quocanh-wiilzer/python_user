568fe84e115a3757cdb5ba93b2e23120484783ad
quocanh-wiilzer <quoc.anh.vien.01031992@gmail.com>
2024-07-27 21:58:28 +1000

    Add files via upload

---
+++ data.py
@@ -0,0 +1,102 @@
+import imaplib
+import email
+import sys
+import pytz
+import datetime
+
+timezone = pytz.timezone('US/Pacific')
+
+today = datetime.datetime.now(timezone)
+
+today = today.strftime("%d-%b-%Y")
+
+imap_server = "imap.mail.yahoo.com"
+email_address = "quocanh.vien@yahoo.com"
+password = "gnfimcldthrpqkim"
+
+
+query = f"(SUBJECT \"Your Transport for NSW account verification code\" SINCE \"{today}\")"
+query_sms = f"(SUBJECT \"Forward SMS From: Microsoft\" SINCE \"{today}\")"
+
+status=""
+code=""
+get_sms=""
+contents=[]
+sms=[]
+
+
+def check_email():
+    try:
+        global contents
+        contents=[]
+        imap = imaplib.IMAP4_SSL(imap_server)
+        imap.login(email_address, password)
+        imap.select("Inbox")
+        _, msgnums = imap.search(None, query)
+        for msgnum in msgnums[0].split():
+            _, data = imap.fetch(msgnum, "(RFC822)")
+            message = email.message_from_bytes(data[0][1])
+            for part in message.walk():
+                if part.get_content_type() == "text/plain":
+                    my_email = email.message_from_string(part.as_string())
+                    content=my_email.get_payload(decode=True).decode(sys.stdout.encoding)
+                    contents.append(content)
+        imap.close()
+        contents.reverse()
+        return contents[0]
+
+    except Exception as e:
+        print(e)
+        print("Something went wrong!")
+
+
+def check_email_sms():
+    try:
+        global sms
+        sms=[]
+        imap = imaplib.IMAP4_SSL(imap_server)
+        imap.login(email_address, password)
+        imap.select("Inbox")
+        _, msgnums = imap.search(None, query_sms)
+        for msgnum in msgnums[0].split():
+            _, data = imap.fetch(msgnum, "(RFC822)")
+            message = email.message_from_bytes(data[0][1])
+            for part in message.walk():
+                if part.get_content_type() == "text/plain":
+                    my_email = email.message_from_string(part.as_string())
+                    content=my_email.get_payload(decode=True).decode(sys.stdout.encoding)
+                    sms.append(content)
+        imap.close()
+        sms.reverse()
+        return sms[0]
+    except Exception as e:
+        print(e)
+        print("Something went wrong!")
+
+
+
+def get_email_code():
+    try:
+        global code
+        code = check_email()
+        if code is not None:
+            sorted_code = code[171:190]
+            return sorted_code
+        else:
+            return "Email has not been sent by RMS :("
+    except Exception as e:
+        return str(e)
+
+def get_sms_code():
+    try:
+        global get_sms
+        get_sms = check_email_sms()
+        if get_sms is not None:
+            sorted_sms = get_sms[38:45]
+            return sorted_sms
+        else:
+            return "SMS has not been sent by RMS :("
+
+    except Exception as e:
+        return str(e)
+
---
+++ main.py
@@ -0,0 +1,130 @@
+import streamlit as st
+import time as ts
+#import pyautogui as autogui
+import data
+
+
+st.markdown("""
+<style>
+.st-emotion-cache-h4xjwg.ezrtsby2
+{
+    visibility:hidden
+}
+</style>
+""", unsafe_allow_html=True)
+
+    #autogui.hotkey("ctrl","F5")
+
+
+@st.cache_data(show_spinner=False)
+def pro_bar_email():
+    bar = st.progress(0)
+    progress_status = st.empty()
+    for i in range(100):
+        value = (i + 1)
+        bar.progress(value)
+        progress_status.write(str(i + 1) + "%")
+        ts.sleep(0.20)
+
+@st.cache_data(show_spinner=False)
+def pro_bar_sms():
+    bar = st.progress(0)
+    progress_status = st.empty()
+    for i in range(100):
+        value = (i + 1)
+        bar.progress(value)
+        progress_status.write(str(i + 1) + "%")
+        ts.sleep(0.20)
+
+email_text="Code will be shown here"
+sms_text="Code will be shown here"
+
+
+
+if "email_clicked" not in st.session_state:
+    st.session_state.email_clicked = False
+
+
+if "sms_clicked" not in st.session_state:
+    st.session_state.sms_clicked = False
+
+
+c1,c2=st.columns([5,3])
+c1.header("AIS - Code Scrapper")
+message=st.empty()
+st.divider()
+
+
+col1, col2, col3=st.columns([3,1,1])
+
+col3.write("")
+col3.write("")
+
+if col3.button("Reset And Try Again"):
+    st.cache_data.clear()
+    st.session_state.email_clicked = False
+    st.session_state.sms_clicked = False
+    st.experimental_rerun()
+
+
+with col1:
+    st.caption("Email Verification code:")
+    email=st.empty()
+    status1 = st.empty()
+    btn_email=st.empty()
+    sms_cap=st.empty()
+    sms=st.empty()
+    status2 = st.empty()
+    btn_sms=st.empty()
+
+
+    def create_email_code(text):
+        with status1.container():
+            pro_bar_email()
+        email.code(data.get_email_code())
+
+    def create_sms_code(text):
+        with status2.container():
+            pro_bar_sms()
+        sms.code(data.get_sms_code())
+
+
+    def email_callback():
+        global email_text
+        st.session_state.email_clicked = True
+        email_text = ""
+        create_email_code(email_text)
+
+
+    def sms_callback():
+        global sms_text
+        st.session_state.sms_clicked = True
+        sms_text = ""
+        create_sms_code(sms_text)
+
+    email.code(email_text)
+    email_click=btn_email.button("Get Email Code",on_click=email_callback)
+    if email_click or st.session_state.email_clicked:
+        email_callback()
+        btn_email.write("")
+        sms.code(sms_text)
+        sms_cap.caption("SMS Verification code:")
+        sms_click=btn_sms.button("Get SMS Code",on_click=sms_callback)
+        if sms_click or st.session_state.sms_clicked:
+            sms_callback()
+            btn_sms.write("")
+            message.success("If Code Were Not Loaded Successfully, Try [Reset And Try Again] Button Or Try Contact Brian O'Conner")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
